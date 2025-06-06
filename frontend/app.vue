edda62f48c367f724a310abae486a71be9275533
kaan <kaandesu00@gmail.com>
2025-06-06 18:26:18 +0200

    feat(wip/page navigation): added state-manager store to handle routing logic

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,7 +1,23 @@
 <template>
-  <NuxtLayout>
-    <NuxtLoadingIndicator height="5" />
+  <NuxtLayout :class="theme">
+    <NuxtLoadingIndicator :color="theme" :height="5" />
     <NuxtPage />
     <Toaster />
   </NuxtLayout>
 </template>
+<script setup lang="ts">
+import { Toaster } from "@/components/ui/sonner";
+import { useStateManager, theme } from "@/stores/state-manager";
+
+const app = useStateManager();
+const { updateActivePage } = app;
+const route = useRoute();
+
+onMounted(() => {
+  updateActivePage(route.name);
+});
+watch(
+  () => route.name,
+  () => updateActivePage(route.name),
+);
+</script>
