bd6d0b2782c1568cb9afe62a3b390f48465039a5
kaan <kaandesu00@gmail.com>
2025-06-07 01:28:52 +0200

    feat(layouts): created deafult & with-tabs layout

--- frontend/layouts/main.vue
+++ frontend/layouts/main.vue
@@ -6,23 +6,36 @@
         <section
           class="box-border flex h-16 w-full items-center justify-between px-6 py-4"
         >
-          <div />
+          <div
+            v-auto-animate
+            class="flex flex-col justify-center items-start pt-5 gap-x-2"
+          >
+            <div class="text-3xl font-bold">
+              {{ currentPageInfo.title }}
+            </div>
+            <div class="">
+              <p v-if="currentPageInfo.desc">
+                {{ currentPageInfo.desc }}
+                <br />
+              </p>
+            </div>
+          </div>
           <div class="flex items-center justify-center gap-x-2">
             <LangSelect />
             <DarkButton />
+            <NuxtLink
+              to="https://github.com/kaandesu/yomuhoudai"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button class="rounded-full" size="icon" variant="ghost">
+                <Icon name="mdi:github" size="1.5rem" />
+              </Button>
+            </NuxtLink>
           </div>
         </section>
         <!-- Page  -->
         <section class="box-border h-[calc(100%-4rem)] w-full p-6 pt-2">
-          <div class="text-3xl font-bold">
-            {{ currentPageInfo.title }}
-          </div>
-          <div v-auto-animate class="pb-2">
-            <p v-if="currentPageInfo.desc">
-              {{ currentPageInfo.desc }}
-              <br />
-            </p>
-          </div>
           <slot />
         </section>
       </div>
