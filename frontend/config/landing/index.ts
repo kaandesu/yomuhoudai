bf8997c36eda837cd63296f9efe6a02ff40c0cd9
kaan <kaandesu00@gmail.com>
2025-06-06 17:33:45 +0200

    fix(landing-page): remove missing config

--- frontend/config/landing/index.ts
+++ frontend/config/landing/index.ts
@@ -2,7 +2,6 @@ import { benefits } from "./benefits";
 import { features } from "./features";
 import { howitworks } from "./howitworks";
 import { hero } from "./hero";
-import { services } from "./services";
 import { team } from "./team";
 import { testimonials } from "./testimonials";
 
@@ -10,10 +9,9 @@ export const landing = {
   Hero: hero,
   Benefits: benefits,
   Features: features,
-  Services: services,
   HowItWorks: howitworks,
   Testimonials: testimonials,
   Team: team,
 };
 
-export { benefits, features, howitworks, hero, services, testimonials, team };
+export { benefits, features, howitworks, hero, testimonials, team };
