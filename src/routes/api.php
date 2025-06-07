06d7da54728310f0d3cddabd2cb2bbaae71de8df
kaan <kaandesu00@gmail.com>
2025-06-07 15:16:03 +0200

    feat(api/wip): added books api

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,18 +1,41 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::middleware('auth:api')->get(
+    '/user',
+    function (Request $request) {
+        return $request->user();
+    }
+);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::prefix('v1')->
+    group(
+        function () {
+            Route::get('/books', 'BookController@index');
+            Route::get('/books/{id}', 'BookController@show');
+            Route::post('/books', 'BookController@store');
+            Route::put('/books/{id}', 'BookController@update');
+            Route::delete('/books/{id}', 'BookController@destroy');
+
+            Route::get('/books/search/title', 'BookController@searchTitle');
+            Route::get('/books/search/author', 'BookController@searchAuthor');
+
+            // TODO: Uncomment when implemented
+            // Route::get('/books/export/csv/{type}/{search?}', 'BookController@exportToCSV');
+            // Route::get('/books/export/xml/{type}/{search?}', 'BookController@exportToXML');
+        }
+    );
+
+Route::get(
+    '/status',
+    function () {
+        return response()->json(
+            [
+            'status' => 'ok',
+            'timestamp' => now()->toDateTimeString()
+            ]
+        );
+    }
+);
