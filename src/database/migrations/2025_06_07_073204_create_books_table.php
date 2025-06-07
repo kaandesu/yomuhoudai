2a620ecf0eebd5f45a5a0a4294dab1a4814e98a1
kaan <kaandesu00@gmail.com>
2025-06-07 15:13:50 +0200

    chore(migration): added books table migration

---
+++ src/database/migrations/2025_06_07_073204_create_books_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'books',
+            function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('title');
+                $table->string('author');
+                $table->string('currentPage')->nullable();
+                $table->string('cover')->nullable();
+                $table->text('description')->nullable();
+                $table->json('categories')->nullable();
+                $table->string('pageCount')->nullable();
+                $table->string('language')->nullable();
+                $table->string('publisher')->nullable();
+                $table->string('publishedDate')->nullable();
+                $table->string('averageRating')->nullable();
+                $table->string('ratingsCount')->nullable();
+                $table->enum('status', ['completed', 'ongoing', 'on-hold', 'plan-to-read', 'dropped'])->nullable();
+                $table->timestamps();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
