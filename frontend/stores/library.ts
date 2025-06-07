7bd90bf73b031388859bb35a36cd259c74b86d46
kaan <kaandesu00@gmail.com>
2025-06-08 01:04:50 +0200

    feat(store/library): initial api manager store implementation

--- frontend/stores/library.ts
+++ frontend/stores/library.ts
@@ -1,9 +1,11 @@
 import { defineStore } from "pinia";
 
 export type Book = {
+  id: number;
   title: string;
   author: string;
   cover?: string;
+  currentPage?: string;
   description?: string;
   categories?: string[];
   pageCount?: string;
@@ -13,13 +15,37 @@ export type Book = {
   averageRating?: string;
   ratingsCount?: string;
   added?: boolean;
-  status: "completed" | "ongoing" | "on-hold" | "plan-to-read" | "dropped";
+  status?: "completed" | "ongoing" | "on-hold" | "plan-to-read" | "dropped";
 };
+
+interface BookResponse {
+  code: number;
+  data: Book | null;
+  message?: string;
+}
+
+type BooksResponse = Book[];
+
+type Callbacks = {
+  onSuccess?: (...args: unknown[]) => unknown;
+  onError?: (...args: unknown[]) => unknown;
+};
+
+export type Route =
+  | `/v1/books`
+  | `/v1/books/${number}`
+  | `/v1/books/search/title`
+  | `/v1/books/search/author`
+  | `/v1/books/export/csv/${string}/${string | undefined}`
+  | `/v1/books/export/xml/${string}/${string | undefined}`;
+
+const backendUrl = "http://localhost:80/api";
 
 export const useLibrary = defineStore(
   "Library",
   () => {
-    const books = ref<Book[]>([
+    const books = ref<Book[]>([]);
+    const books2 = ref<any>([
       {
         title: "To Kill a Mockingbird",
         author: "Harper Lee",
@@ -242,11 +268,186 @@ export const useLibrary = defineStore(
       },
     ]);
 
-    return { books };
+    const loading = ref<boolean>(false);
+
+    const getBooks = async ({ onSuccess, onError }: Callbacks = {}) => {
+      loading.value = true;
+      const { data } = await useFetch<BooksResponse>("/api/v1/books", {
+        server: false,
+      });
+
+      loading.value = false;
+      if (data.value != null) {
+        books.value = data.value;
+      }
+
+      if (data.value) {
+        if (onSuccess) onSuccess(data.value);
+      } else {
+        if (onError) onError(data.value);
+      }
+    };
+
+    const getBookById = async ({
+      id,
+      onSuccess,
+      onError,
+    }: { id: number } & Callbacks) => {
+      const { data, pending } = await useFetch<BookResponse>(
+        `/v1/books/${id}`,
+        {
+          method: "GET",
+          headers: {},
+        },
+      );
+
+      loading.value = pending.value;
+
+      if (data.value?.code === 200 && data.value.data) {
+        if (onSuccess) onSuccess(data.value.data);
+      } else {
+        if (onError) onError(data.value?.code ?? -1, data.value?.message);
+      }
+    };
+
+    const createBook = async ({
+      book,
+      onSuccess,
+      onError,
+    }: { book: Omit<Book, "id"> } & Callbacks) => {
+      const { data, pending } = await useFetch<BookResponse>(
+        backendUrl + "/v1/books",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(book),
+        },
+      );
+
+      loading.value = pending.value;
+
+      if (data.value?.code === 200 && data.value.data) {
+        if (onSuccess) onSuccess(data.value.data);
+      } else {
+        if (onError) onError(data.value?.code ?? -1, data.value?.message);
+      }
+    };
+
+    const updateBook = async ({
+      id,
+      book,
+      onSuccess,
+      onError,
+    }: { id: number; book: Partial<Omit<Book, "id">> } & Callbacks) => {
+      const { data, pending } = await useFetch<BookResponse>(
+        `/v1/books/${id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(book),
+        },
+      );
+
+      loading.value = pending.value;
+
+      if (data.value?.code === 200 && data.value.data) {
+        if (onSuccess) onSuccess(data.value.data);
+      } else {
+        if (onError) onError(data.value?.code ?? -1, data.value?.message);
+      }
+    };
+
+    const deleteBook = async ({
+      id,
+      onSuccess,
+      onError,
+    }: { id: number } & Callbacks) => {
+      loading.value = true;
+
+      const { data } = await useFetch<{
+        code: number;
+        message?: string;
+      }>(`/api/v1/books/${id}`, {
+        method: "DELETE",
+        server: false,
+      });
+
+      loading.value = false;
+
+      if (data.value?.code === 200 || data.value === null) {
+        await getBooks();
+        if (onSuccess) onSuccess();
+      } else {
+        if (onError) onError(data.value?.code ?? -1, data.value?.message);
+      }
+    };
+
+    const searchBooksByTitle = async ({
+      title,
+      onSuccess,
+      onError,
+    }: { title: string } & Callbacks) => {
+      const query = encodeURIComponent(title);
+      const { data, pending } = await useFetch<BooksResponse>(
+        `/v1/books/search/title?title=${query}`,
+        {
+          method: "GET",
+          headers: {},
+        },
+      );
+
+      loading.value = pending.value;
+
+      if (data.value != null) {
+        if (onSuccess) onSuccess(data.value);
+      } else {
+        if (onError) onError(data.value);
+      }
+    };
+
+    const searchBooksByAuthor = async ({
+      author,
+      onSuccess,
+      onError,
+    }: { author: string } & Callbacks) => {
+      const query = encodeURIComponent(author);
+      const { data, pending } = await useFetch<BooksResponse>(
+        `/v1/books/search/author?author=${query}`,
+        {
+          method: "GET",
+          headers: {},
+        },
+      );
+
+      loading.value = pending.value;
+
+      if (data.value != null) {
+        if (onSuccess) onSuccess(data.value);
+      } else {
+        if (onError) onError(data.value);
+      }
+    };
+
+    return {
+      loading,
+      books,
+      getBooks,
+      getBookById,
+      createBook,
+      updateBook,
+      deleteBook,
+      searchBooksByTitle,
+      searchBooksByAuthor,
+    };
   },
   {
     persist: {
-      paths: ["books"],
+      // TODO: uncomment
+      // paths: ["books"],
     },
   },
 );
