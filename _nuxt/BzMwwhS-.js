cf4296b60d055b970cfe35407994553274029988
kaandesu <kaandesu@users.noreply.github.com>
2025-06-09 19:15:22 +0000

    deploy: 1d48c71f851b13f5f711ca1a72fe459eb1ac65f8

---
+++ _nuxt/BzMwwhS-.js
@@ -0,0 +1,6 @@
+import{c as r}from"./CiOEmGEh.js";import{_ as a}from"./CWmmVa7x.js";import{g as n,c as t,o as c,n as p,j as m,C as i}from"./CpOUWE1v.js";/**
+ * @license lucide-vue-next v0.483.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const _=r("ArrowRightIcon",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]]),h=n({__name:"CardFooter",props:{class:{}},setup(s){const o=s;return(e,l)=>(c(),t("div",{class:p(("cn"in e?e.cn:m(a))("flex items-center p-6 pt-0",o.class))},[i(e.$slots,"default")],2))}});export{_ as A,h as _};
