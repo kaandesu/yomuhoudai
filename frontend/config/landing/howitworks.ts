74f6b73375fd2768f9052ff80734d52df35cf568
kaan <kaandesu00@gmail.com>
2025-06-19 14:02:40 +0200

    chore: update the landing page

--- frontend/config/landing/howitworks.ts
+++ frontend/config/landing/howitworks.ts
@@ -14,28 +14,28 @@ export const howitworks = {
       title: "Easily Add Books to Your Library",
       description:
         "Search or manually add your favorite books to your personal library with just a few clicks.",
-      image: "https://picsum.photos/200",
+      image: "./books.webp",
     },
     {
       badgeTitle: "Track Progress",
       title: "Monitor Your Reading Journey",
       description:
         "Update your reading status, mark pages read, and watch your progress grow over time.",
-      image: "https://picsum.photos/201",
+      image: "./change-status.webp",
     },
     {
       badgeTitle: "Get Recommendations",
       title: "Discover Books Tailored to You",
       description:
         "Receive personalized book suggestions based on your reading history and preferences.",
-      image: "https://picsum.photos/202",
+      image: "./explore.webp",
     },
     {
-      badgeTitle: "Share & Connect",
-      title: "Share Your Journey with Friends",
+      badgeTitle: "Find your book",
+      title: "Search & Sort ",
       description:
-        "Show off your reading milestones and connect with friends to exchange book ideas.",
-      image: "https://picsum.photos/203",
+        "Search by title or author, sort results, paginate, and download them as needed.",
+      image: "./search.webp",
     },
   ] as HowItWorksProps[],
 };
